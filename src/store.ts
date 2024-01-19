42de5b7d6ada3122e691078ef38d9d0dba6c92b7
Abdurrahman <timur.navrazov@mail.ru>
2024-01-19 15:32:38 +0300

    changed! now without 'on' with 'sample'

--- src/store.ts
+++ src/store.ts
@@ -1,4 +1,4 @@
-import {createEvent, createStore} from "effector";
+import {createEvent, createStore, sample} from "effector";
 
 export interface Todo {
     id: number;
@@ -40,28 +40,81 @@ export const update = createEvent<{id: number, text:string}>();
 export const toggle = createEvent<number>();
 export const remove = createEvent<number>();
 
-export default createStore<Store>({
+const todosStore = createStore<Store>({
     todos: [],
     newTodo: ''
 })
-.on(setNewTodo, (state, newTodo) => ({
+// .on(setNewTodo, (state, newTodo) => ({
+//     ...state,
+//     newTodo
+// }))
+// .on(addTodo, (state) => ({
+//     ...state,
+//     newTodo: '',
+//     todos: addTodoList(state.todos, state.newTodo)
+// }))
+// .on(update, (state, {id, text}) => ({
+//     ...state,
+//     todos: updateTodo(state.todos, id, text)
+// }))
+// .on(toggle, (state, id) => ({
+//     ...state,
+//     todos: toggleTodo(state.todos, id)
+// }))
+// .on(remove, (state, id) => ({
+//     ...state,
+//     todos: removeTodo(state.todos, id)
+// }))
+
+sample({
+  source: todosStore,
+  clock: setNewTodo,
+  target: todosStore,
+  fn: (state, newTodo) => ({
     ...state,
-    newTodo
-}))
-.on(addTodo, (state) => ({
+    newTodo,
+  }),
+});
+
+sample({
+  source: todosStore,
+  clock: addTodo,
+  target: todosStore,
+  fn: (state) => ({
     ...state,
-    newTodo: '',
-    todos: addTodoList(state.todos, state.newTodo)
-}))
-.on(update, (state, {id, text}) => ({
+    newTodo: "",
+    todos: addTodoList(state.todos, state.newTodo),
+  }),
+});
+
+sample({
+  source: todosStore,
+  clock: update,
+  target: todosStore,
+  fn: (state, { id, text }) => ({
     ...state,
-    todos: updateTodo(state.todos, id, text)
-}))
-.on(toggle, (state, id) => ({
+    todos: updateTodo(state.todos, id, text),
+  }),
+});
+
+sample({
+  source: todosStore,
+  clock: toggle,
+  target: todosStore,
+  fn: (state, id) => ({
     ...state,
-    todos: toggleTodo(state.todos, id)
-}))
-.on(remove, (state, id) => ({
+    todos: toggleTodo(state.todos, id),
+  }),
+});
+
+sample({
+  source: todosStore,
+  clock: remove,
+  target: todosStore,
+  fn: (state, id) => ({
     ...state,
-    todos: removeTodo(state.todos, id)
-}))
+    todos: removeTodo(state.todos, id),
+  }),
+});
+
+export default todosStore
